a14c2c2eb77295e5439bff2f5eecf3fe97505578
'borisgr04@hotmail.com' <admin_sena@SENA>
2015-08-24 22:28:36 -0500

    se confirma

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         echo "Esta es una prueba de php";
+        
         // put your code here
         echo phpinfo();
         ?>
